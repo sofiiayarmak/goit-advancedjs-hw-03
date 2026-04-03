8e0fe570c6c8339fc1cd542729a9e628314087b0
Sofiia <sophia.yarmak@gmail.com>
2026-04-03 21:09:56 +0200

    axios and gallery

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_KEY = '55294732-f0b081769c244f5ac0d2772ff';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  return axios
+    .get(BASE_URL, { params })
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+      throw error;
+    });
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,55 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+        </a>
+        <div class="info">
+          <p><b>Likes:</b> ${likes}</p>
+          <p><b>Views:</b> ${views}</p>
+          <p><b>Comments:</b> ${comments}</p>
+          <p><b>Downloads:</b> ${downloads}</p>
+        </div>
+      </li>
+    `;
+      }
+    )
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('hidden');
+}
